3287a95e2dcab1797b2f982fba0c5957b6f1fe53
vladimir2019kovalenko <46881132+vladimir2019kovalenko@users.noreply.github.com>
2019-06-28 11:21:20 +1000

    Update MortgateCalculator.js

--- instruments/mortgage/js/MortgateCalculator.js
+++ instruments/mortgage/js/MortgateCalculator.js
@@ -1,9 +1,41 @@
-const MortgateCalculator = () => (
-  <div>
-    <form action="">
-      <Autocomplete/>
-      <Inputs/>
-      <Button/>
-    </form>
-  </div>
-);
+const {Form} = window.antd;
+
+const formItemLayout =  {
+      labelCol: { span: 8 },
+      wrapperCol: { span: 15 }
+    }
+
+class MortgateCalculator extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      apartment: '',
+      price: '',
+      money: '',
+      duration: ''
+    }
+  }
+
+  onChange(value, name) {
+    this.setState({[name]: value})
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <Row justify='center' type='flex'>
+        <Form layout="horizontal" onSubmit={this.onSubmit}>
+          <Autocomplete/>
+          <Inputs/>
+          <Col offset='10'>
+            <ButtonTest/>
+          </Col>
+        </Form>
+      </Row>
+    )
+  }
+}
