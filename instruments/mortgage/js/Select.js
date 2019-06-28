22046053538b231641ef4b585251e7679e96390c
vladimir2019kovalenko <46881132+vladimir2019kovalenko@users.noreply.github.com>
2019-06-28 11:22:30 +1000

    Update Select.js

--- instruments/mortgage/js/Select.js
+++ instruments/mortgage/js/Select.js
@@ -1,10 +1,17 @@
-const Autocomplete = () => (
-  <div>
-    Тип квартиры:<br/>
-    <select>
-      <option>Квартира в новостройке</option>
-      <option>Готовая квартира</option>
-      <option>Загородный дом</option>
-    </select>
-   </div>
-);
+const {Select} = window.antd;
+const Option = Select.Option;
+
+const Autocomplete = (props) => {
+  return (
+    <Form.Item label='Тип квартиры:' {...formItemLayout}>
+      <Select
+        style={{ width: 200 }}
+        autofocus
+        placeholder="Выберите тип жилья">
+        <Option value="new_building">Квартира в новостройке</Option>
+        <Option value="finished_building">Готовая квартира</Option>
+        <Option value="countryhouse">Загородный дом</Option>
+      </Select>
+    </Form.Item>
+  )
+}
